a4fcab76563472070d161ad40e83badf8a272884
Nhu Le <lekhaquynhnhu2805@gmail.com>
2025-03-21 12:55:47 +0700

    new

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 const port = 5000;
 
 let resultFromClient = null;
